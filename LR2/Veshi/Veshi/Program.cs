fc6b9eb2d9b5cf6eddb8b8687df9a208995c2b86
st56 <zahark456@gmail.com>
2025-04-24 13:16:35 +0300

    Коменты

--- LR2/Veshi/Veshi/Program.cs
+++ LR2/Veshi/Veshi/Program.cs
@@ -10,33 +10,53 @@ namespace Veshi
     {
         static void Main(string[] args)
         {
-            //Создается новый экземпляр класса
+            // Создаем новый объект класса SaleList для хранения информации о продажах
             SaleList report = new SaleList();
-            //новый объект класса Sales в AddSale
+
+            // Добавляем несколько продаж в отчет:
+            // Каждая продажа содержит:
+            // - номер продавца (1 или 2)
+            // - объект Things (товар) с параметрами:
+            //   * название товара
+            //   * цена
+            //   * количество
+            //   * бренд (из перечисления Vid_odezhdi)
+
+            
             report.AddSale(new Sales(1, new Things("Сумка", 1500, 12, Vid_odezhdi.Zadidas)));
-            report.AddSale(new Sales(1, new Things("Футболка", 1700, 6, Vid_odezhdi.Zike)));
-
+            report.AddSale(new Sales(1, new Things("Футболка", 1000, 6, Vid_odezhdi.Zike)));
             report.AddSale(new Sales(1, new Things("джинсы", 2000, 8, Vid_odezhdi.Zadidas)));
+            report.AddSale(new Sales(1, new Things("Штаны", 2000, 5, Vid_odezhdi.Zuma)));
+            report.AddSale(new Sales(1, new Things("джемпер", 3550, 13, Vid_odezhdi.Zalenciaga)));
+            report.AddSale(new Sales(1, new Things("кепка", 3500, 9, Vid_odezhdi.Zuma)));
+
+
             report.AddSale(new Sales(2, new Things("Штаны", 1000, 5, Vid_odezhdi.Zuma)));
-
             report.AddSale(new Sales(2, new Things("джемпер", 350, 13, Vid_odezhdi.Zalenciaga)));
-            report.AddSale(new Sales(2, new Things("кепка", 500, 9, Vid_odezhdi.Zuma)));
+            report.AddSale(new Sales(2, new Things("кепка", 1500, 9, Vid_odezhdi.Zuma)));
+            report.AddSale(new Sales(2, new Things("Сумка", 1500, 12, Vid_odezhdi.Zadidas)));
+            report.AddSale(new Sales(2, new Things("Футболка", 1900, 6, Vid_odezhdi.Zike)));
+            report.AddSale(new Sales(2, new Things("джинсы", 3000, 8, Vid_odezhdi.Zadidas)));
 
-
-
+            // Запрашиваем у пользователя выбор типа отчета
             int reportType;
             Console.WriteLine("Выберите вид отчёта (1 - весь, 2 - по видам): ");
             reportType = Convert.ToInt32(Console.ReadLine());
+
+            // В зависимости от выбора пользователя выводим соответствующий отчет
             if (reportType == 1)
             {
+                // Выводим полный отчет (все продажи)
                 report.PrintReportAll();
             }
             else if (reportType == 2)
             {
+                // Выводим отчет, сгруппированный по видам одежды/брендам
                 report.PrintReportPerGenre();
             }
             else
             {
+                // Обработка некорректного ввода
                 Console.WriteLine("Введено неверное значение");
             }
         }
--- LR3/LR3/MainForm.cs
+++ LR3/LR3/MainForm.cs
@@ -12,69 +12,90 @@ namespace LR3
 {
     public partial class MainForm : Form
     {
-        Dictionary<string, List<string>> menu; // Словарь для хранения групп медикаментов и их списка 
+        // Словарь для хранения меню: ключ - группа блюд (например, "Закуски"), значение - список блюд в этой группе
+        Dictionary<string, List<string>> menu;
 
+        // Словарь для хранения заказа: 
+        // ключ 1-го уровня - группа блюд, 
+        // ключ 2-го уровня - название блюда, 
+        // значение - количество этого блюда в заказе
         Dictionary<string, Dictionary<string, int>> order = new Dictionary<string, Dictionary<string, int>>();
-        // Словарь для хранения выбранных медикоментов и их количества
 
         public MainForm()
         {
-            InitializeComponent();
-            InitializeMedication();
-            InitializeData();
-
+            InitializeComponent(); // Стандартная инициализация компонентов формы
+            InitializeMedication(); // Инициализация данных меню
+            InitializeData(); // Настройка элементов управления
         }
+
+        // Метод для инициализации данных меню
         private void InitializeMedication()
         {
+            // Создание структуры меню с группами блюд и их списками
             menu = new Dictionary<string, List<string>>
-            {
-                { "Закуски", new List<string>{"Салат Цезарь", "Сырные палочки", "Гренки чесночные"}},
-                { "Основные блюда", new List<string>{"Стейк", "Паста Карбонара", "Пицца Маргарита"}},
-                 {"Напитки", new List<string>() {"Кола", "Сок апельсиновый", "Вода"}},
-                { "Десерты", new List<string>{"Тирамису", "Чизкейк", "Мороженое"}}
-            };
+        {
+            { "Закуски", new List<string>{"Салат Цезарь", "Сырные палочки", "Гренки чесночные"}},
+            { "Основные блюда", new List<string>{"Стейк", "Паста Карбонара", "Пицца Маргарита"}},
+            {"Напитки", new List<string>() {"Кола", "Сок апельсиновый", "Вода"}},
+            { "Десерты", new List<string>{"Тирамису", "Чизкейк", "Мороженое"}}
+        };
 
+            // Инициализация структуры заказа для каждой группы меню
             foreach (var group in menu.Keys)
             {
                 order[group] = new Dictionary<string, int>();
             }
         }
+
+        // Метод для настройки элементов управления на форме
         private void InitializeData()
         {
+            // Заполнение listBox_groups названиями групп меню
             foreach (var group in menu.Keys)
             {
                 listBox_groups.Items.Add(group);
             }
 
-            listBox_groups.SelectedIndexChanged += listBox_groups_SelectedIndexChanged;
-            button_AddToZakaz.Click += button_AddToZakaz_Click;
+            // Подписка на события
+            listBox_groups.SelectedIndexChanged += listBox_groups_SelectedIndexChanged; // При изменении выбранной группы
+            button_AddToZakaz.Click += button_AddToZakaz_Click; // При нажатии кнопки "Добавить в заказ"
         }
-        //отвечает за заполнение списка блюд
+
+        // Обработчик события изменения выбранной группы в listBox_groups
         private void listBox_groups_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Очистка панели перед добавлением новых элементов
             SelectionPanel_flowLayoutPanel.Controls.Clear();
-            // Получаем выбранную группу меню
+
+            // Получение выбранной группы из listBox
             string SelectedGroup = listBox_groups.SelectedItem.ToString();
-            // Заполняем блюдами из выбранной группы
+
+            // Для каждого блюда в выбранной группе
             foreach (var blu in menu[SelectedGroup])
             {
+                // Создание CheckBox для выбора блюда
                 CheckBox checkBox = new CheckBox { Text = blu };
+                // Создание NumericUpDown для выбора количества
                 NumericUpDown numericUpDown = new NumericUpDown { Minimum = 0, Maximum = 100, Width = 50 };
 
+                // Если блюдо уже есть в заказе, устанавливаем соответствующие значения
                 if (order[SelectedGroup].ContainsKey(blu))
                 {
                     numericUpDown.Value = order[SelectedGroup][blu];
                     checkBox.Checked = true;
                 }
+
                 // Обработчик изменения состояния CheckBox
                 checkBox.CheckedChanged += (s, ev) =>
                 {
                     if (checkBox.Checked)
                     {
+                        // Если блюдо выбрано, добавляем его в заказ с текущим количеством
                         order[SelectedGroup][blu] = (int)numericUpDown.Value;
                     }
                     else
                     {
+                        // Если сняли выбор, удаляем блюдо из заказа
                         order[SelectedGroup].Remove(blu);
                     }
                 };
@@ -82,33 +103,44 @@ namespace LR3
                 // Обработчик изменения значения NumericUpDown
                 numericUpDown.ValueChanged += (s, ev) =>
                 {
+                    // Если блюдо выбрано, обновляем его количество в заказе
                     if (checkBox.Checked)
                     {
                         order[SelectedGroup][blu] = (int)numericUpDown.Value;
                     }
                 };
 
+                // Добавление элементов на панель
                 SelectionPanel_flowLayoutPanel.Controls.Add(checkBox);
                 SelectionPanel_flowLayoutPanel.Controls.Add(numericUpDown);
             }
         }
 
+        // Обработчик нажатия кнопки "Добавить в заказ"
         private void button_AddToZakaz_Click(object sender, EventArgs e)
         {
-            string orders = "";
+            string orders = ""; // Строка для формирования текста заказа
+
+            // Перебор всех групп меню
             foreach (string group in menu.Keys)
             {
-                orders += $"{group}:\n";
+                orders += $"{group}:\n"; // Добавляем название группы
+
                 if (order[group].Count > 0)
                 {
+                    // Добавляем все блюда из этой группы, которые есть в заказе
                     foreach (var blu in order[group])
                     {
-                        orders += $"{blu.Key} x {blu.Value}\n";
+                        orders += $"{blu.Key} x {blu.Value}\n"; // Название блюда и количество
                     }
                 }
-                else { orders += "-------------- \n"; }
+                else
+                {
+                    orders += "-------------- \n"; // Если в группе ничего не заказано
+                }
             }
 
+            // Выводим сформированный заказ в richTextBox1
             richTextBox1.Text = orders;
         }
     }
